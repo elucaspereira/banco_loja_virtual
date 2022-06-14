3f0c8de38812a51adf1544a87799d8e9e13b7917
Lucas Pereira <lucas_rm35@hotmail.com>
2022-06-13 23:03:44 -0300

    commit
    criando msg de erro e atualizando informações no produto

--- loja-virtual/cloud/main.js
+++ loja-virtual/cloud/main.js
@@ -5,11 +5,31 @@ Parse.Cloud.define("hello", (request) => {
  });
 
 Parse.Cloud.define("create-product", async (request) => {
+  const stock = request.params.stock;
+  const price = request.params.price;
+  if(stock == null || stock > 999) throw "Deve ser informando uma quantidade válida para o campo estoque";
+  if(stock < 0) throw "Estoque nao pode ser negativo";
+  if(price == null || stock < 0) throw "O preço informado é inválido"
   const product = new Product();
-  product.set("name", "Placa Mãe asus h410-m");
-  product.set("price", 1800);
-  product.set("stock", 20);
-  product.set("isSelling",true);
+  product.set("name", request.params.name); //passando a requisiçao por parametro para adicionar os dados por Json
+  product.set("price", request.params.price);
+  product.set("stock", request.params.stock );
+  product.set("isSelling", request.params.isSelling);
   const savedProduct = await product.save(null, {useMasterKey: true});
-  return savedProduct
+  return savedProduct.id
+});
+
+//alterar somente campos que for expecificados os demasi ficaram bloqueados
+Parse.Cloud.define("change-price", async (request) => {
+  const id = request.params.productId;
+  const price = request.params.price;
+  if(id == null) throw "Informe o ID do produto";
+  if(price == null) throw "Prenecha o  Campo Preço"
+
+  const product = new Product();
+  product.id = request.params.productId;
+  product.set("price", request.params.price);
+  const savedProduct = await product.save(null,{useMasterKey:true});
+  return savedProduct.get("price");
+
 });
