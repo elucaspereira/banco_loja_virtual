e3a1210fcc3a989ff5d232c1e243c1a4b437f244
Lucas Pereira <lucas_rm35@hotmail.com>
2022-06-14 22:44:48 -0300

    commit
    atualizando produtos e deletando

--- loja-virtual/cloud/main.js
+++ loja-virtual/cloud/main.js
@@ -33,3 +33,28 @@ Parse.Cloud.define("change-price", async (request) => {
   return savedProduct.get("price");
 
 });
+
+//funçao para atualizar todas as informaçoes dde um produto 
+Parse.Cloud.define("update-products", async (request) => {
+  const id = request.params.productId;
+  if(id == null) throw "Deve ser informado o id do produto"
+
+  const product = new Product();
+  product.id = request.params.productId;
+  product.set("name", request.params.name);
+  product.set("price", request.params.price);
+  product.set("stock", request.params.stock);
+  product.set("isSelling", request.params.isSelling);
+  const savedProduct = await product.save(null, {useMasterKey: true});
+  return "O Pruduto foi atualizado"; 
+});
+
+//deletar um produto
+Parse.Cloud.define("delete-product", async (request) => {
+  if(request.params.productId == null) throw 'Produto invalido';
+  const product = new Product();
+  product.id = request.params.productId
+
+  await product.destroy({useMasterKey: true});
+  return "Pruduto excluído com sucesso";
+});
